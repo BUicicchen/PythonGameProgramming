4c9a9ba69c15398c4bd04b07d38247ae3fc63ca0
icicchen <cicichen07040805@gmail.com>
2016-09-05 20:16:14 +0800

    This program determines if the input number is within 100~110 or 200~210. If yes, then the statement is true, if not, then it is false.

---
+++ True or False.py
@@ -0,0 +1,8 @@
+'''
+This program determines if the input number is within 100~110 or 200~210.
+If yes, then the statement is true, if not, then it is false.
+
+'''
+n = input("Please enter a number: ")
+n = float(n)
+n >= 100 and n<=110 or n >= 200 and n<= 210
