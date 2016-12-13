e3fce0d48858f5cecc5a8cab403f90dcddae9328
icicchen <cicichen07040805@gmail.com>
2016-12-13 23:59:38 +0800

    menu words program

--- menu words.py
+++ menu words.py
@@ -21,15 +21,22 @@ for w in glossary:
     if '</pre' in w:
         finish = glossary.index(w)
         print(finish)
-narnya = glossary[start:finish]
+
+
+'''
+for index, word in enumerate(narnya):
+    lowerCase = word.lower()
+    for character in lowerCase:
+        if character in ['!.;:,"?/=>1234567890()-']:
+            lowerCase.replace(character, '')
+    narnya[index] = lowerCase.replace(character,'')
 print(narnya)
 
 
+'''
 
-lwidth = 20
-rwidth = 20
-title = "The Chronicles of Narnia"
-def printMenu(lwidth, rwidth, title):
+
+def printMenu(lwidth, rwidth, title, items):
     #prints out the word and its frequency of appearing
     """
     Prints a dictionary's keys and values in a menu format
@@ -41,14 +48,77 @@ def printMenu(lwidth, rwidth, title):
     """
     x = title
     print(x.center(lwidth+rwidth, "-"))
+    for k, v in items.items():
+        print(k.ljust(lwidth, ".") + str(v).rjust(rwidth, "."))
+    return items
 
+narnya = glossary[start:finish]
+#convert words to lower case
+lowerCase = [word.lower() for word in narnya]
+
+#take out all punctuations
+for word in lowerCase:
+    for characters in word:
+        if characters in ['!.;:,"?/=>1234567890()-']:
+            word.replace(characters, '')
+'''
     countWord = {}
-    for word in glossary:
+    for word in narnya:
         if word in countWord:
             countWord[word] += 1
         else:
             countWord[word] = 1
-    for k, v in countWord.items():
-        print(k.ljust(lwidth, ".") + str(v).rjust(rwidth, "."))
+'''
 
-menu = printMenu(lwidth, rwidth, title)
+def bubbleSort(item):
+    #Impliment of bubble sort, return the sorted bank of words
+    '''
+    :param item: list
+    :param index:
+    :param worldbanklist = list of unsorted tuples
+    :return: list of tuples
+    '''
+    index = list(range(len(item)))
+    for i in range(len(item)):
+        for j in range(len(item)-i-1):
+            if item[j] > item[j+1]:
+                item[j], item[j+1] = item[j+1], item[i]
+                index[j], index[j+1] = item[j+1], index[j]
+    return item, index
+
+
+def prebubble(wordbanklist):
+    items = [element[1] for element in wordbanklist]
+    items_sort, index = bubbleSort(items)
+    sortwordbank = [wordbanklist[i] for i in index]
+    return sortwordbank
+
+def insertionSort(items):
+    #Impliment of insertion sort, return the sorted bank of words, the faster way
+    '''
+    :param items:
+    :return: dictionary
+    '''
+    for i in range(1, len(items)):
+        j = i
+        while j > 0 and items[j] < items[j-1]:
+            items[j], items[j-1] = items[j-1], items[j]
+            j -= 1
+    return items
+
+insertionSort(items=[])
+
+i = {}
+for j in lowerCase[1:100]:
+    if 'x' not in i.keys():
+        i[j] = lowerCase.count(j)
+
+wordbank = printMenu(20, 20, "The Chronicles of Narnia", i)
+
+wblist = [(k,v) for k, v in wordbank.items()]
+
+
+
+#sortlist = prebubble(wblist[1:50])
+sortlist = insertionSort(wblist[1:50])
+print(wordbank)
